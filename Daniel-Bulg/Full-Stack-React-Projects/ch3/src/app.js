dc3f60e0a355647544c06788db8e2db08c8397a6
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-06-10 14:03:57 -0300

    finalizado ch3

--- Daniel-Bulg/Full-Stack-React-Projects/ch3/src/app.js
+++ Daniel-Bulg/Full-Stack-React-Projects/ch3/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 import { postsRoutes } from './routes/posts.js'
 
 const app = express()
+app.use(cors)
 app.use(bodyParser.json())
 postsRoutes(app)
 
