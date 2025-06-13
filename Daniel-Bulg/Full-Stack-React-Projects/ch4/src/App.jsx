39b6c8a905fb222ba825d79560dbb4cfb3716dd0
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-06-13 10:09:11 -0300

    fiinalizado components

--- Daniel-Bulg/Full-Stack-React-Projects/ch4/src/App.jsx
+++ Daniel-Bulg/Full-Stack-React-Projects/ch4/src/App.jsx
@@ -1,5 +1,31 @@
+import { PostList } from './components/PostLists.jsx'
 import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
+
+const posts = [
+  {
+    title: 'FullStack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'DevCorrea',
+  },
+  {
+    title: 'Hello React!',
+  },
+]
 
 export function App() {
-  return <CreatePost />
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter By:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
 }
