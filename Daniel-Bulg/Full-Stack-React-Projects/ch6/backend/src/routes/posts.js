d5d4c02421d7fd253425ad0d299908a2c3a8df2c
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-06-16 10:21:08 -0300

    autenticação

--- Daniel-Bulg/Full-Stack-React-Projects/ch6/backend/src/routes/posts.js
+++ Daniel-Bulg/Full-Stack-React-Projects/ch6/backend/src/routes/posts.js
@@ -7,6 +7,7 @@ import {
   updatePost,
   deletePost,
 } from '../services/posts.js'
+import { requireAuth } from '../middleware/jwt.js'
 
 export function postsRoutes(app) {
   app.get('/api/v1/posts', async (req, res) => {
@@ -43,9 +44,9 @@ export function postsRoutes(app) {
     }
   })
 
-  app.post('/api/v1/posts', async (req, res) => {
+  app.post('/api/v1/posts', requireAuth, async (req, res) => {
     try {
-      const post = await createPost(req.body)
+      const post = await createPost(req.auth.sub, req.body)
       return res.json(post)
     } catch (error) {
       console.error('error creating post', error)
@@ -53,9 +54,9 @@ export function postsRoutes(app) {
     }
   })
 
-  app.patch('/api/v1/posts/:id', async (req, res) => {
+  app.patch('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
-      const post = await updatePost(req.params.id, req.body)
+      const post = await updatePost(req.auth.sub, req.params.id, req.body)
       return res.json(post)
     } catch (error) {
       console.error('error updating post', error)
@@ -63,9 +64,9 @@ export function postsRoutes(app) {
     }
   })
 
-  app.delete('/api/v1/posts/:id', async (req, res) => {
+  app.delete('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
-      const { deletedCoount } = await deletePost(req.params.id)
+      const { deletedCoount } = await deletePost(req.auth.sub, req.params.id)
       if (deletedCoount === 0) return res.status(404)
       return res.status(204).end()
     } catch (error) {
